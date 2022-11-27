53d0bdf04f070d49b9dd8282ef1842813a642020
tim7l <tim7l@Nitro5.fritz.box>
2022-11-27 16:17:24 +0100

    HW7_Exercise2

---
+++ test123/src/HW7_Exercise2/LeafTeam.java
@@ -0,0 +1,63 @@
+package HW7_Exercise2;
+
+public class LeafTeam implements Team{
+	
+	private String name;
+	private int nrOfAthletes;
+	private int nrOfGoldMedals;
+	
+	public LeafTeam(String name, int nrAthletes, int nrGoldMedals) {
+		this.name = name;
+		this.nrOfAthletes = nrAthletes;
+		this.nrOfGoldMedals = nrGoldMedals;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getNrOfAthletes() {
+		return this.nrOfAthletes;
+	}
+	
+	public int getNrOfGoldMedals() {
+		return this.nrOfGoldMedals;
+	}
+	
+	public void addGoldMedal(int nr) {
+		this.nrOfGoldMedals += nr;
+	}
+	
+	public void addGoldMedal() {
+		this.nrOfGoldMedals++;
+	}
+	
+	public void addAthlete(int nr) {
+		this.nrOfAthletes += nr;
+	}
+	
+	public void addAthlete() {
+		this.nrOfAthletes++;
+	}
+	
+	public void removeAthlete(int nr) {
+		if(this.nrOfAthletes >= nr) {
+			this.nrOfAthletes -= nr;
+		}else {
+			System.out.println("There are already less than " + nr + " Athletes in this Team. Did nothing.");
+		}
+		
+	}
+
+	@Override
+	public void nameAndAthletes() {		
+		System.out.println("  Team: " + this.name + ", number of Athletes: " + this.nrOfAthletes);
+		
+	}
+
+	@Override
+	public void nameAndGoldMedals() {	
+		System.out.println("  Team: " + this.name + ", number of gold medals: " + this.nrOfGoldMedals);
+		
+	}
+}
---
+++ test123/src/HW7_Exercise2/Team.java
@@ -0,0 +1,8 @@
+package HW7_Exercise2;
+
+public interface Team {
+	
+	public void nameAndAthletes();
+	public void nameAndGoldMedals();
+
+}
